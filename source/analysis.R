e657dcd163d87292895045679bb484c328be215c
jndoan <jamesdoan123@gmail.com>
2022-02-25 02:26:16 -0800

    update

--- source/analysis.R
+++ source/analysis.R
@@ -6,7 +6,7 @@ library("ggplot2")
 county_level_data <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
 jurisdiction_level_data <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends_jail_jurisdiction.csv")
 
-# Summary Statistic 1
+# Summary Statistic 1, change of jailed population of latinx folks between 2017 and 2018
 
 
 # Summary Statistic 2
