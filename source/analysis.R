c189edee2f48e970d48d20c12e1cc6225cad9cd8
jndoan <jamesdoan123@gmail.com>
2022-02-25 01:38:51 -0800

    updates

---
+++ source/analysis.R
@@ -0,0 +1,32 @@
+library("dplyr")
+library("tidyr")
+library("ggplot2")
+
+
+county_level_data <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+jurisdiction_level_data <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends_jail_jurisdiction.csv")
+
+# Summary Statistic 1
+
+
+# Summary Statistic 2
+
+
+# Summary Statistic 3
+
+
+# Summary Statistic 4
+
+
+# Summary Statistic 5
+
+
+# Time Trend Chart would be a total population over an amount of time
+
+
+# Variable Comparison compare two races
+
+
+# Map
+
+
